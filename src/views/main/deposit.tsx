e6412bce14f2960876f9b75d7489cfb17a5b7894
BigBen3918 <benjamin199551@gmail.com>
2022-10-19 01:10:34 -0700

    update: all

--- src/views/main/deposit.tsx
+++ src/views/main/deposit.tsx
@@ -1,16 +1,24 @@
 import React from "react";
+import QRCode from "react-qr-code";
 import Layout from "../../components/main";
 import { copyToClipboard } from "../../utils";
 
 export default function Deposit() {
-    const [copy, setCopy] = React.useState(false);
+    const [flag, setFlag] = React.useState(0);
+    const [copyFlag, setCopyFlag] = React.useState(false);
+
+    const PaymentMethod = [
+        { name: "Bitcoin", address: String(process.env.REACT_APP_BTCADDRESS) },
+        { name: "Ether", address: String(process.env.REACT_APP_ETHADDRESS) },
+        { name: "USDT", address: String(process.env.REACT_APP_USDTADDRESS) },
+    ];
 
     const copyAddress = () => {
-        copyToClipboard(String(process.env.REACT_APP_COMADDRESS))
+        copyToClipboard(PaymentMethod[flag].address)
             .then(() => {
-                setCopy(true);
+                setCopyFlag(true);
                 setTimeout(() => {
-                    setCopy(false);
+                    setCopyFlag(false);
                 }, 3000);
             })
             .catch((err: any) => {
@@ -27,13 +35,34 @@ export default function Deposit() {
                     to send Bitcoin.
                 </p>
                 <div className="spacer-30"></div>
-                <span onClick={copyAddress}>
-                    {process.env.REACT_APP_COMADDRESS}
-                </span>
-                <p style={{ color: "var(--secondary)" }}>
-                    {copy ? "Copied address" : null}
-                </p>
-                <div className="spacer-30"></div>
+                <div className="payment_method">
+                    {PaymentMethod.map((title: any, index: number) => (
+                        <div
+                            onClick={() => setFlag(index)}
+                            className={flag === index ? "active" : undefined}
+                            key={index}
+                        >
+                            {title.name}
+                        </div>
+                    ))}
+                </div>
+                <div className="spacer-50"></div>
+                <div className="payment_code">
+                    <QRCode
+                        size={256}
+                        className="qrcode"
+                        value={PaymentMethod[flag].address}
+                        viewBox={`0 0 256 256`}
+                    />
+                    <span onClick={copyAddress}>
+                        {PaymentMethod[flag].address}
+                    </span>
+                </div>
+                <div className="spacer-20"></div>
+                <h6 style={{ color: "var(--secondary)" }}>
+                    {copyFlag ? "Copied address!!!" : ""}
+                </h6>
+                <div className="spacer-20"></div>
                 <p>
                     Your trading account will be credited once your payment is
                     fully confirmed.
